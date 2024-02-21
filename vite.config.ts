1e7fb5d7bac7f855e4fd5e4158369d599718cb62
Oneclassick <36659331+Oneclassick15@users.noreply.github.com>
2024-02-21 18:09:53 +0200

    added base param

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mist-feedback-form/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
